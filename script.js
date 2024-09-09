c1ba577b5b5df4762521114b3502e9317d49b2a8
Mr. Algorithm <11aravipratapsingh@gmail.com>
2024-09-09 16:43:33 +0530

    Project completed

---
+++ script.js
@@ -0,0 +1,30 @@
+var tl = gsap.timeline();
+
+tl.from("#nav h2, #nav h3",{
+    y: -50,
+    opacity: 0,
+    delay: 0.4,
+    duration: 0.8,
+    stagger: 0.3
+})
+
+tl.from("#content h1",{
+    x: -250,
+    opacity: 0,
+    duration: 0.8,
+    stagger: 0.3
+})
+
+tl.from("#footer",{
+    opacity: 0,
+    y: 50,
+    duration: 0.4,
+})    
+
+tl.from("img",{
+    x: 100,
+    rotate: 45,
+    opacity: 0,
+    duration: 0.5,
+    stagger: 0.3
+})
